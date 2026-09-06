927942f407d93ff371fb737aa346491e9cc4adaa
Manthan <manthan3996@gmail.com>
2026-09-06 17:32:20 +0530

    Deleted Unnecessary tests and files

--- tests/PlaywrightPractice2.spec.js
+++
@@ -1,8 +0,0 @@
-const {test,expect} = require('@playwright/test')
-
-test("Practice WEb Automation 1", async ({page})=>{
-    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-    await page.locator('#userEmail').fill('mtms@yopmail.com')
-    await page.locator("#userPassword").fill('Admin@123456')
-    
-})
