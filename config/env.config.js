9f7c0da76fb11abfba33c16e799862e80bbb99e5
Manthan <manthan3996@gmail.com>
2026-09-06 18:00:33 +0530

    Latest Commits and Push

--- config/env.config.js
+++ config/env.config.js
@@ -1,18 +1,24 @@
+const dotenv = require('dotenv');
+
+if (!process.env.CI) {
+    dotenv.config();
+}
+
 const environments = {
     dev: {
-        baseUrl: 'https://rahulshettyacademy.com/client'
+        baseUrl: 'https://rahulshettyacademy.com/client/'
     },
 
     qa: {
-        baseUrl: 'https://rahulshettyacademy.com/client'
+        baseUrl: 'https://rahulshettyacademy.com/client/'
     },
 
     uat: {
-        baseUrl: 'https://rahulshettyacademy.com/client'
+        baseUrl: 'https://rahulshettyacademy.com/client/'
     },
 
     prod: {
-        baseUrl: 'https://rahulshettyacademy.com/client'
+        baseUrl: 'https://rahulshettyacademy.com/client/'
     }
 };
 
@@ -20,14 +26,13 @@ const environment = process.env.ENV || 'qa';
 
 if (!environments[environment]) {
     throw new Error(
-        `Invalid environment: ${environment}. Use dev, qa, uat or prod.`
+        `Invalid environment: ${environment}. Valid values: dev, qa, uat, prod`
     );
 }
 
 module.exports = {
     environment,
-    ...environments[environment],
-
+    baseUrl: environments[environment].baseUrl,
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD
 };
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,4 @@
-const LOGIN_PATH = '/#/auth/login'
+const LOGIN_PATH = '#/auth/login'
 class LoginPage{
 
     constructor(page) {
