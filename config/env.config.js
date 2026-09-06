374b7d7a446b2513901b773ab5ac4c00897ca629
Manthan <manthan3996@gmail.com>
2026-09-06 17:20:03 +0530

    LAtest changes

--- config/env.config.js
+++ config/env.config.js
@@ -1,13 +1,33 @@
-const dotenv = require('dotenv');
+const environments = {
+    dev: {
+        baseUrl: 'https://rahulshettyacademy.com/client'
+    },
 
-if (!process.env.CI) {
-    dotenv.config();
+    qa: {
+        baseUrl: 'https://rahulshettyacademy.com/client'
+    },
+
+    uat: {
+        baseUrl: 'https://rahulshettyacademy.com/client'
+    },
+
+    prod: {
+        baseUrl: 'https://rahulshettyacademy.com/client'
+    }
+};
+
+const environment = process.env.ENV || 'qa';
+
+if (!environments[environment]) {
+    throw new Error(
+        `Invalid environment: ${environment}. Use dev, qa, uat or prod.`
+    );
 }
 
-const config = {
-    baseUrl: process.env.BASE_URL,
+module.exports = {
+    environment,
+    ...environments[environment],
+
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD
 };
-
-module.exports = config;
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,5 @@
-class LoginPage {
+const LOGIN_PATH = '/#/auth/login'
+class LoginPage{
 
     constructor(page) {
 
@@ -13,7 +14,7 @@ class LoginPage {
 
     async navigateToLoginPage() {
 
-        await this.page.goto('/');
+        await this.page.goto(LOGIN_PATH);
 
     }
 
